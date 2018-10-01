7af9591a19774377fa007e3caf9a044d81e83c83
Peter Sculley <32631294+peteresculley@users.noreply.github.com>
2018-10-01 00:48:14 -0600

    Basic information added to display

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 const { app, BrowserWindow } = require('electron');
 
+let win;
+
 function createWindow() {
 	win = new BrowserWindow({
+		show: false,
 		width: 800,
 		height: 600
 	});
 	
 	win.loadFile('index.html');
+	
+	//win.webContents.openDevTools();
+	
+	win.on('ready-to-show', () => {
+		win.show();
+	});
+	
+	win.on('closed', () => {
+		win = null;
+	});
 }
 
 app.on('ready', createWindow);
+
+// for mac
+app.on('window-all-closed', () => {
+	if(process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+// for mac
+app.on('activate', () => {
+	if(win === null) {
+		createWindow();
+	}
+});
